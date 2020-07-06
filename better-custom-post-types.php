06b204b5a3e8a1892caafc42705938df55a01af4
Steven Quinn <steven@figoliquinn.com>
2020-07-06 12:25:12 -0700

    Update better-custom-post-types.php

--- better-custom-post-types.php
+++ better-custom-post-types.php
@@ -3,10 +3,8 @@
   Plugin Name: Better Custom Post Types
   Author: Figoli Quinn & Associates
   Description: Helper for registering custom post types.
-  Version: 1.0
+  Version: 0.1
 */
-
-use FigoliQuinn\BetterCustomPostTypes\WPUpdater;
 
 if ( !defined( 'ABSPATH' ) ) exit;
 
@@ -14,5 +12,5 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 if ( is_plugin_active( 'wp-update-from-github/wp-update-from-github.php' ) ) {
     // Allow updating from github repository.
-    new WPUpdater;
+    new FigoliQuinn\BetterCustomPostTypes\WPUpdater;
 }
